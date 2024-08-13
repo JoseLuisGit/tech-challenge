c46b9bcfe7890d3240ffd2b32776bea52ea8a078
Jose Luis Quispe <joseluisstacruz@gmail.com>
2024-08-13 07:11:43 -0400

    remove time

--- scripts/docker_stop_and_rm.sh
+++ scripts/docker_stop_and_rm.sh
@@ -2,7 +2,7 @@
 container_id=$(ssh -o StrictHostKeyChecking=no ${AWS_EC2_USER}@${AWS_EC2_HOST} docker ps -a -q -f "name=^${DEPLOYMENT_CONTAINER_NAME}$")
 
 if [ -n "${container_id}" ]; then
-    ssh -o StrictHostKeyChecking=no ${AWS_EC2_USER}@${AWS_EC2_HOST} docker stop --time 1000 ${container_id}
+    ssh -o StrictHostKeyChecking=no ${AWS_EC2_USER}@${AWS_EC2_HOST} docker stop ${container_id}
     ssh -o StrictHostKeyChecking=no ${AWS_EC2_USER}@${AWS_EC2_HOST} docker rm -f ${container_id}
     echo "[+] ${DEPLOYMENT_CONTAINER_NAME} stopped and removed in ${AWS_EC2_HOST}"
 fi
