b6d7cb65f865f7224a298ce7f59a2d41fc268396
Jose Luis Quispe <joseluisstacruz@gmail.com>
2024-08-12 23:02:18 -0400

    improvements in deploy actions

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,7 +1,7 @@
 ssh -o StrictHostKeyChecking=no ${AWS_EC2_USER}@${AWS_EC2_HOST} docker run -d \
           -p 80:8000 \
-          --env-file ./.env
-          -v ./.env:/app/.env
+          --env-file ${GITHUB_WORKSPACE}/.env
+          -v ${GITHUB_WORKSPACE}/.env:/app/.env
           --restart unless-stopped \
           --name ${DEPLOYMENT_CONTAINER_NAME}  \
           ${DEPLOYMENT_CONTAINER_NAME}:${DEPLOYMENT_IMAGE_TAG}
